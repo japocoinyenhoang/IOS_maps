9b3deb1fc6b4e59c2cdd353cdc59a928111c236e
japocoinyenhoang <japocoin@gmail.com>
2021-04-27 18:43:30 +0200

    first commit

---
+++ maps/mapsTests/mapsTests.swift
@@ -0,0 +1,33 @@
+//
+//  mapsTests.swift
+//  mapsTests
+//
+//  Created by Tardes on 27/4/21.
+//
+
+import XCTest
+@testable import maps
+
+class mapsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
